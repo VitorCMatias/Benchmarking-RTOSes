491f051a8d2c4476bc0355505264f8bc21b8aac5
Vitor <vitorcosta.matias@usp.br>
2022-04-26 15:13:06 -0300

    Toggle com freeRTOS
    Faltava iniciar o scheaduler e o periodo de pulso deveria ser alterado para 250ms

--- Com_FreeRTOS/toggle_com_freeRTOS/toggle_com_freeRTOS.ino
+++ Com_FreeRTOS/toggle_com_freeRTOS/toggle_com_freeRTOS.ino
@@ -1,45 +1,29 @@
-#include <FreeRTOS.h>
-#include <task.h>
+#include "FreeRTOS.h"
+#include "task.h"
 
-#define PORTA_EXPERIMENTO 42  
+#include "GPIO.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  xTaskCreate(blink, (const portCHAR *)"blink", 128, NULL, 2, NULL);
+  vTaskStartScheduler();
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
 
 void blink(void *pvParameters)
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  
-  for( ;; ) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    vTaskDelay(1000/ portTICK_PERIOD_MS);
-    digitalWrite(LED_BUILTIN, LOW);
-    vTaskDelay(1000/ portTICK_PERIOD_MS);
+  GPIO porta_experimento(42, OUTPUT);
+  GPIO LED(LED_BUILTIN, OUTPUT);
+
+  for ( ;; ) {
+    porta_experimento.toggle();
+    LED.toggle();
   }
-}
-
-
-void toggle(void *pvParameters)
-{
-  pinMode(PORTA_EXPERIMENTO, OUTPUT);
-  
-  for( ;; ) {
-    digitalWrite(PORTA_EXPERIMENTO, HIGH);
-    vTaskDelay(1000/ portTICK_PERIOD_MS);
-    digitalWrite(PORTA_EXPERIMENTO, LOW);
-    vTaskDelay(1000/ portTICK_PERIOD_MS);
-  }
-}
-
-void setup() 
-{
-  Serial.begin(9600);
-
-  xTaskCreate(blink, (const portCHAR *)"blink", 128, NULL, 1, NULL);
-  xTaskCreate(toggle, (const portCHAR *)"toggle_gpio", 128, NULL, 2, NULL);
-  vTaskStartScheduler();
-
-  Serial.println("Failed to start FreeRTOS scheduler");
-  while(1);
-}
-
-void loop()
-{
 }
